eef50273bfc249ea8151a5dfe4c92609aaa7d520
Jyootimoy kakoty <jyootimoy.kakoty@gmail.com>
2021-02-20 19:30:09 +0530

    Transfer Money, Also updated a few previous modules

--- LogInScript.js
+++ LogInScript.js
@@ -5,7 +5,9 @@ const userGreet = document.querySelector('.currentUserGreet');
 const logInForm = document.querySelector('.logIn__form');
 const LogInPass = document.querySelector('.LogIn__input-pass');
 
+
 let currentUser = null;
+let bal = (accumulator, currentValue) => accumulator + currentValue;
 
 const users = new Map([
     ['jyootimoy', '2021'],
@@ -53,7 +55,7 @@ const displayDashboard = function(){
     var summary = document.createElement('div');
     summary.classList.add('summary');
     var transactions = document.createElement('div');
-    transactions.classList.add('transactios');
+    transactions.classList.add('transactions');
     var transfer = document.createElement('div');
     transfer.classList.add('transfer');
     var loan = document.createElement('div');
@@ -63,39 +65,107 @@ const displayDashboard = function(){
     statement.classList.add('statement');
     var balance = document.createElement('div');
     balance.classList.add('balance');
-    var currentdate = new Date(); 
-    var datetime = "As of: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-    const bal = (accumulator, currentValue) => accumulator + currentValue;
-    const balOutput = currentUser.transaction.reduce(bal);
-    statement.innerHTML = "<h1>Balance</h1>" + datetime;
-    balance.innerHTML = `<h1>₹ ${balOutput}</h1> A/C: ${currentUser.accountNo}`;
+
     document.body.appendChild(summary);
     summary.appendChild(statement);
     summary.appendChild(balance);
+    getSummary(summary);
 
     getTransactions(transactions);
     document.body.appendChild(transactions);
+
+    initTransfer(transfer);
     document.body.appendChild(transfer);
+
     document.body.appendChild(loan);
-    //
+    checkAction();
 
 }
 
+var datetime = function() {
+    var currentdate = new Date(); 
+    return currentdate.getDate() + "/"
+            + (currentdate.getMonth()+1)  + "/" 
+            + currentdate.getFullYear() + " @ "  
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+}
+
+const getSummary = function(summary) {
+    const statement = document.querySelector('.statement');
+    const balance = document.querySelector('.balance');
+    //summary.innerHTML = "";
+    //statement.innerHTML = "";
+    //balance.innerHTML = "";
+    
+    const balOutput = currentUser.transaction.reduce(bal);
+    statement.innerHTML = "<h1>Balance</h1>" + "As of: " + datetime();
+    balance.innerHTML = `<h1>₹ ${balOutput}</h1> A/C: ${currentUser.accountNo}`;
+}
+
 const getTransactions = function(transactions) {
+    //transactions.innerHTML = "";
     currentUser.transaction.forEach((element, i) => {
         const type = element > 0 ? 'Deposit' : 'Withdrawl';
         const row = `
             <div class="row">
                 <div class="${type}">${type}</div>
-                <div class="value">₹ ${element}</div>
+                <div class="transDate"><i>${datetime()}</i></div>
+                <div class="value"> ₹ ${element}</div>
             </div>
         `;
         transactions.insertAdjacentHTML('afterbegin', row);
-        
+    });
+}
+
+const initTransfer = function(transfer) {
+    const form = `
+        <form class="Transfer__form">
+            <div style="text-align: center; width: 100%; font-size: 2.5rem;">Transfer Money</div>                    
+            <button class="Transfer__btn">&rarr;</button>
+            <input class="TransferAmount" type="text" maxlength="16" required/>
+            <input class="TransferTo" type="text" maxlength="16" required/>
+        </form>
+        <div style="margin-left: 7rem; display: inline-block; width: 40%; font-size: 2rem;">Transfer To</div>
+        <div style="margin: 0; display: inline-block; width: 40%; font-size: 2rem;">Amount</div>
+    `;
+    transfer.insertAdjacentHTML('afterbegin', form);
+}
+
+const checkAction = function() {
+    const TransferBtn = document.querySelector('.Transfer__btn');
+    const TransferAmount = document.querySelector('.TransferAmount');
+    const TransferTo = document.querySelector('.TransferTo');
+    TransferBtn.addEventListener('click', function(e){
+        e.preventDefault();
+        console.log(currentUser);
+        const balOutput = currentUser.transaction.reduce(bal);
+        console.log(TransferTo.value);
+        let accountValidity = 0;
+        let transferNow = 0;
+        for(const [key, {accountNo, transaction}] of account) {
+            console.log(accountNo, TransferTo.value);
+            if(accountNo === TransferTo.value && currentUser.accountNo !== TransferTo.value) {
+                accountValidity = 1;
+                transferNow = transaction;
+                console.log(`To transfer: ${accountNo}`);
+            }
+        }
+        if(accountValidity === 0)  {alert('Invalid Account No'); return;}
+        console.log(`Amount ${TransferAmount.value}`);
+        if(TransferAmount.value <= 0) {
+            alert('Enter Valid Amount'); return;
+        }
+        else {
+            balOutput >= TransferAmount.value
+            ? currentUser.transaction.push(-1 * TransferAmount.value)
+            && transferNow.push(Number(TransferAmount.value)) && alert('Transfer Successful')
+            : alert('Low Balance!');
+            const summary = document.querySelector('.summary');
+            getSummary(summary);
+            const transactions = document.querySelector('.transactions');
+            getTransactions(transactions);
+        }
     });
 }
